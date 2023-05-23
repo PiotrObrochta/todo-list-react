edddd378fbd5c304628d789691d8c0687e81d56b
PiotrObrochta <piotr.obrochta88@gmail.com>
2023-05-23 11:11:21 +0200

    I'm starting to port my app to React

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main classNameName="container">
+        <header>
+            <h1>Lista zadań</h1>
+        </header>
+        </section>
+        <section className="section">
+            <h2 className="section__header">Dodaj nowe zadanie</h2>
+            <div className="section__body">
+                <form className="form js-form">
+                    <input placeholder="Co jest do zrobienia?" className="form__field js-newTask" autofocus>
+                    <button className="form__button">Dodaj zadanie</button>
+            </div>
+            </form>
+        </section>
+        <section className="section">
+            <header className="section__header section__taskListTitleButtons">
+            <h2>Lista zadań</h2>
+            <div className="section__button js-buttons"></div>
+            </header>
+            <div className="section__body">
+                <ul className="task js-tasks"></ul>
+            </div>
+        </section>
+    </main>
   );
 }
 
