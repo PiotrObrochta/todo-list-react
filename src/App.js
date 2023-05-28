6b7f8526920c279df154f4842ffb088a74722b69
PiotrObrochta <piotr.obrochta88@gmail.com>
2023-05-28 19:52:17 +0200

    build Form

--- src/App.js
+++ src/App.js
@@ -1,31 +1,33 @@
+import Form from "./Form";
 
+const tasks = [
+    { id: 1, content: "przejść na Reacta", done: false },
+    { id: 2, content: "zjeść obiad", done: true },
+];
+
+const hideDoneTasks = false;
 
 function App() {
-  return (
-    <main classNameName="container">
-        <header>
-            <h1>Lista zadań</h1>
-        </header>
-        </section>
-        <section className="section">
-            <h2 className="section__header">Dodaj nowe zadanie</h2>
-            <div className="section__body">
-                <form className="form js-form">
-                    <input placeholder="Co jest do zrobienia?" className="form__field js-newTask" autofocus>
-                    <button className="form__button">Dodaj zadanie</button>
-            </div>
-            </form>
-        </section>
-        <section className="section">
-            <header className="section__header section__taskListTitleButtons">
-            <h2>Lista zadań</h2>
-            <div className="section__button js-buttons"></div>
+    return (
+    <main className="container">
+            <header>
+                <h1>Lista zadań</h1>
             </header>
-            <div className="section__body">
-                <ul className="task js-tasks"></ul>
+            <section className="section">
+                <h2 className="section__header">Dodaj nowe zadanie</h2>
+                <div className="section__body">
+                    <Form />
             </div>
-        </section>
-    </main>
+        </section><section className="section">
+                <header className="section__header section__taskListTitleButtons">
+                    <h2>Lista zadań</h2>
+                    <div className="section__button"></div>
+                </header>
+                <div className="section__body">
+                    <ul className="task"></ul>
+                </div>
+            </section>
+    </main >
   );
 }
 
