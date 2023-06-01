59f434746d2a8d63d4f66ea2a4245232850fd5c3
PiotrObrochta <piotr.obrochta88@gmail.com>
2023-06-01 17:44:41 +0200

    change name filed to input

--- src/Form/index.js
+++ src/Form/index.js
@@ -2,7 +2,7 @@ import "./style.css";
 
 const Form = () => (
     <form className="form">
-        <input placeholder="Co jest do zrobienia?" className="form__field" autofocus />
+        <input placeholder="Co jest do zrobienia?" className="form__input" autofocus />
         <button className="form__button">Dodaj zadanie</button>
     </form>
 )
