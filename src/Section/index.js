27f99009dd242562565c63c53e282a4bbf9d5a13
PiotrObrochta <piotr.obrochta88@gmail.com>
2023-06-12 18:53:13 +0200

    add section

--- src/Section/index.js
+++ src/Section/index.js
@@ -1,9 +1,15 @@
 import "./style.css";
 
-const Section = (props) => (
-<section>
-
-</section>
+const Section = ({ title, body, extraHeaderContent }) => (
+    <section className="section">
+        <header className="section__header section__taskListTitleButtons">
+            <h2>{title}</h2>
+            {extraHeaderContent}
+        </header>
+        <div className="section__body">
+            {body}
+        </div>
+    </section>
 );
 
 export default Section;
