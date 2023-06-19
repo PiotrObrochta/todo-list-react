0e6244c452d5566006a72f0fe0c77bf4b9ff3912
PiotrObrochta <piotr.obrochta88@gmail.com>
2023-06-19 18:05:56 +0200

    Updates

---
+++ precache-manifest.2c42f5b35cf480f13a54afd180a2ade6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "e51c3b322a8bd7e8dde9",
+    "url": "/static/css/main.fcfbfd1e.chunk.css"
+  },
+  {
+    "revision": "e51c3b322a8bd7e8dde9",
+    "url": "/static/js/main.e51c3b32.chunk.js"
+  },
+  {
+    "revision": "9594a6bb494a9d9ba425",
+    "url": "/static/js/1.9594a6bb.chunk.js"
+  },
+  {
+    "revision": "ff813772de7efb309245",
+    "url": "/static/js/2.ff813772.chunk.js"
+  },
+  {
+    "revision": "d9898a2f2791fa6ef766",
+    "url": "/static/js/runtime~main.d9898a2f.js"
+  },
+  {
+    "revision": "ee24961cb0f3d2668712de55b5731c9e",
+    "url": "/index.html"
+  }
+];
