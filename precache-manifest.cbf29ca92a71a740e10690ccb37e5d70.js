6fefa838adc8602c75d606be30e2935081534524
PiotrObrochta <piotr.obrochta88@gmail.com>
2023-06-19 22:36:42 +0200

    Updates

---
+++ precache-manifest.cbf29ca92a71a740e10690ccb37e5d70.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "e51c3b322a8bd7e8dde9",
+    "url": "/todo-list-react/static/css/main.fcfbfd1e.chunk.css"
+  },
+  {
+    "revision": "e51c3b322a8bd7e8dde9",
+    "url": "/todo-list-react/static/js/main.e51c3b32.chunk.js"
+  },
+  {
+    "revision": "9594a6bb494a9d9ba425",
+    "url": "/todo-list-react/static/js/1.9594a6bb.chunk.js"
+  },
+  {
+    "revision": "ff813772de7efb309245",
+    "url": "/todo-list-react/static/js/2.ff813772.chunk.js"
+  },
+  {
+    "revision": "6f3817f9f428eeb5be8c",
+    "url": "/todo-list-react/static/js/runtime~main.6f3817f9.js"
+  },
+  {
+    "revision": "90b9232a41d8f753bc27565527f79cd0",
+    "url": "/todo-list-react/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.2c42f5b35cf480f13a54afd180a2ade6.js"
+  "/todo-list-react/precache-manifest.cbf29ca92a71a740e10690ccb37e5d70.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/todo-list-react/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
