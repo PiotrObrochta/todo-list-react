5fd5a92f16d451dee26f75bb2c00c2e9f8ad87c4
Piotr Obrochta <piotr.obrochta88@gmial.com>
2024-04-15 21:50:50 +0200

    Updates

---
+++ precache-manifest.cf077401317f90037143669e007df2c1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a952f4fa03a55605a04bbbd3ecb7bef",
+    "url": "/todo-list-react/index.html"
+  },
+  {
+    "revision": "8513ae2147c97e21a986",
+    "url": "/todo-list-react/static/css/main.720a5d2f.chunk.css"
+  },
+  {
+    "revision": "645eaca2b5520670e71b",
+    "url": "/todo-list-react/static/js/2.f683a377.chunk.js"
+  },
+  {
+    "revision": "65a9926ecde5afbe3bda",
+    "url": "/todo-list-react/static/js/3.3b6fb9ae.chunk.js"
+  },
+  {
+    "revision": "8513ae2147c97e21a986",
+    "url": "/todo-list-react/static/js/main.a2acab19.chunk.js"
+  },
+  {
+    "revision": "e0b075cdc2f3ff792eff",
+    "url": "/todo-list-react/static/js/runtime~main.f82339de.js"
+  }
+]);
