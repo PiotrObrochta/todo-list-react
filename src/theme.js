e6517a3b561730ba28299401976ccced27f7c15f
Piotr Obrochta <piotr.obrochta88@gmial.com>
2025-10-02 01:29:13 +0200

    created a file in the src folder called theme.js

---
+++ src/theme.js
@@ -0,0 +1,20 @@
+export const theme = {
+    color: {
+        teal:            "#008181ff",
+        darkTeal:        "#006666",
+        tealActive:      "#004c4c",
+        gray:            "#cccccc",
+        borderGray:      "#dddddd",
+        green:           "hsl(122, 65%, 29%)",
+        greenHover:      "hsl(122, 65%, 35%)",
+        greenActive:     "hsl(122, 65%, 40%)",
+        redRemove:       "hsl(354, 84%, 54%)",
+        redRemoveHover:  "hsl(354, 84%, 60%)",
+        redRemoveActive: "hsl(354, 84%, 65%)",
+        black:           "rgba(0, 0, 0, 1)",
+        white:           "rgba(255, 255, 255, 1)",
+    },
+    breakpoints: {
+        mobileMax: "767px",
+    },
+};
