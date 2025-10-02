7940a453ebf32311d3cb53324cfbd77392ade592
Piotr Obrochta <piotr.obrochta88@gmial.com>
2025-10-02 11:18:53 +0200

    color sorting

--- src/theme.js
+++ src/theme.js
@@ -1,20 +1,19 @@
 export const theme = {
-    colors: {
-        teal:            "#008181ff",
-        tealHover:        "#006666",
-        tealActive:      "#004c4c",
+    color: {
+        black:           "rgba(0, 0, 0, 1)",
+        white:           "rgba(255, 255, 255, 1)",
         gray:            "#cccccc",
         borderGray:      "#dddddd",
+        teal:            "#008181ff",
+        tealHover:       "#006666",
+        tealActive:      "#004c4c",
         green:           "hsl(122, 65%, 29%)",
         greenHover:      "hsl(122, 65%, 35%)",
         greenActive:     "hsl(122, 65%, 40%)",
         redRemove:       "hsl(354, 84%, 54%)",
         redRemoveHover:  "hsl(354, 84%, 60%)",
         redRemoveActive: "hsl(354, 84%, 65%)",
-        black:           "rgba(0, 0, 0, 1)",
-        white:           "rgba(255, 255, 255, 1)",
     },
-    
     breakpoints: {
         mobileMax: "767px",
     },
