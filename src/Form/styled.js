a3bdedc958e3e08b1df2c967fe31dd0c4d4131b1
Piotr Obrochta <piotr.obrochta88@gmial.com>
2025-10-02 11:26:31 +0200

    create styled.js on Form

---
+++ src/Form/styled.js
@@ -0,0 +1,34 @@
+import styled from "styled-components";
+
+export const StyledForm = styled.form`
+  display: grid;
+  grid-template-columns: 1fr auto;
+  grid-gap: 20px;
+
+  @media (max-width: ${({ theme }) => theme.breakpoints.mobileMax}) {
+    grid-template-columns: 1fr;
+  }
+`;
+
+export const Input = styled.input`
+  padding: 10px;
+  border: 1px solid ${({ theme }) => theme.color.borderGray};
+
+`;
+
+export const Button = styled.button`
+  padding: 10px;
+  background: ${({ theme }) => theme.color.teal};
+  color: ${({ theme }) => theme.color.white};
+  border: none;
+  transition: 0.3s;
+
+  &:hover {
+    background: ${({ theme }) => theme.color.tealHover};
+    transform: scale(1.1);
+  }
+
+  &:active {
+    background: ${({ theme }) => theme.color.tealActive};
+  }
+`;
