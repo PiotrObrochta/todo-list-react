a5f664949d8f783303e8ef0d070a024e7d4acf7c
Piotr Obrochta <piotr.obrochta88@gmial.com>
2025-10-21 10:51:47 +0200

    Fix TaskList to display task content instead of ID

--- src/features/tasks/TaskList/index.js
+++ src/features/tasks/TaskList/index.js
@@ -1,30 +1,19 @@
 import { List, Item, Content, Button } from "./styled";
 
 const TaskList = ({ tasks, hideDone, removeTask, toggleTaskDone }) => (
-    <List>
-        {tasks.map(task => (
-            <Item
-                key={task.id}
-                hidden={task.done && hideDone}
-            >
-                <Button
-                    $toggleDone
-                    onClick={() => toggleTaskDone(task.id)}
-                >
-                    {task.done ? "✓" : ""}
-                </Button>
-                <Content $done={task.done}>
-                    {task.id} - {task.content}
-                </Content>
-                <Button
-                    $remove
-                    onClick={() => removeTask(task.id)}
-                >
-                    🗑
-                </Button>
-            </Item>
-        ))}
-    </List>
+  <List>
+    {tasks.map((task) => (
+      <Item key={task.id} hidden={task.done && hideDone}>
+        <Button $toggleDone onClick={() => toggleTaskDone(task.id)}>
+          {task.done ? "✓" : ""}
+        </Button>
+        <Content $done={task.done}>{task.content}</Content>
+        <Button $remove onClick={() => removeTask(task.id)}>
+          🗑
+        </Button>
+      </Item>
+    ))}
+  </List>
 );
 
 export default TaskList;
