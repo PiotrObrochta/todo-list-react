755e9174a7b06a96a5713b5e25b68736a0eea8aa
Piotr Obrochta <piotr.obrochta88@gmial.com>
2025-10-23 23:57:24 +0200

    improved code indentation

--- src/common/Section/index.js
+++ src/common/Section/index.js
@@ -1,15 +1,13 @@
 import { FormWrapper, Header, Title, Body } from "./styled";
 
 const Section = ({ title, body, extraHeaderContent }) => (
-    <FormWrapper>
-        <Header>
-            <Title>{title}</Title>
-            {extraHeaderContent}
-        </Header>
-        <Body>
-            {body}
-        </Body>
-    </FormWrapper>
+  <FormWrapper>
+    <Header>
+      <Title>{title}</Title>
+      {extraHeaderContent}
+    </Header>
+    <Body>{body}</Body>
+  </FormWrapper>
 );
 
 export default Section;
