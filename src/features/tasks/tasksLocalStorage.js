8706af2e3b496a758538edf0d6a2e6ad39f832f8
Piotr Obrochta <piotr.obrochta88@gmial.com>
2025-11-14 00:27:48 +0100

    Refactor

--- src/features/tasks/tasksLocalStorage.js
+++ src/features/tasks/tasksLocalStorage.js
@@ -3,12 +3,5 @@ const localStorageKey = "tasks";
 export const saveTasksInLocalStorage = (tasks) =>
   localStorage.setItem(localStorageKey, JSON.stringify(tasks));
 
-export const getTasksFromLocalStorage = () => {
-  try {
-    const tasks = JSON.parse(localStorage.getItem(localStorageKey));
-
-    return Array.isArray(tasks) ? tasks : [];
-  } catch {
-    return [];
-  }
-};
+export const getTasksFromLocalStorage = () =>
+  JSON.parse(localStorage.getItem(localStorageKey)) || [];
