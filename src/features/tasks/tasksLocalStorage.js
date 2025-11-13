48011623c6a225952f8516d8d95e5c4601d0e83e
Piotr Obrochta <piotr.obrochta88@gmial.com>
2025-11-14 00:16:54 +0100

    Fix localStorage implementation

--- src/features/tasks/tasksLocalStorage.js
+++ src/features/tasks/tasksLocalStorage.js
@@ -3,5 +3,12 @@ const localStorageKey = "tasks";
 export const saveTasksInLocalStorage = (tasks) =>
   localStorage.setItem(localStorageKey, JSON.stringify(tasks));
 
-export const getTasksFromLocalStorage = () =>
-  JSON.parse(localStorage.getItem(localStorageKey)) || [];
+export const getTasksFromLocalStorage = () => {
+  try {
+    const tasks = JSON.parse(localStorage.getItem(localStorageKey));
+
+    return Array.isArray(tasks) ? tasks : [];
+  } catch {
+    return [];
+  }
+};
